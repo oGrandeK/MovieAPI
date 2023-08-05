dc87fd729b2bc8eec74ded3a5c5c934b63788590
oGrandeK <kelvimkauam@gmail.com>
2023-08-04 23:55:19 -0300

    Added new method in IMovieRepository

--- MovieAPI.Domain/Interfaces/IMovieRepository.cs
+++ MovieAPI.Domain/Interfaces/IMovieRepository.cs
@@ -7,6 +7,7 @@ public interface IMovieRepository
     Task<IEnumerable<Movie>> GetMoviesDirectorsAsync();
     Task<Movie> GetMovieDirectorsById(int id);
     Task<IEnumerable<Movie>> GetMoviesDirectorsByNameAsync(string movieTitle);
+    Task<IEnumerable<Movie>> GetMoviesDirectorsByGenre(string movieGenre);
     Task<Movie> CreateMovieAsync(Movie movie);
     Task<Movie> UpdateMovieAsync(Movie movie);
     Task<Movie> DeleteMovieAsync(Movie movie);
