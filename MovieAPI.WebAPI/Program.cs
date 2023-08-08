070f4f3dbca84a9efdabf4379f89064ede9ed51c
oGrandeK <kelvimkauam@gmail.com>
2023-08-07 22:00:00 -0300

    Dayli Coding

--- MovieAPI.WebAPI/Program.cs
+++ MovieAPI.WebAPI/Program.cs
@@ -1,6 +1,9 @@
+using MovieAPI.Infraestructure.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
