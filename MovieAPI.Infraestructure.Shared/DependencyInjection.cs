a7556d5363c89269dda30a763afcac0dd74073d1
oGrandeK <kelvimkauam@gmail.com>
2023-08-11 20:22:20 -0300

    Added Repositories to services

--- MovieAPI.Infraestructure.Shared/DependencyInjection.cs
+++ MovieAPI.Infraestructure.Shared/DependencyInjection.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MovieAPI.Domain.interfaces;
 using MovieAPI.Infraestructure.Context;
+using MovieAPI.Infraestructure.Repositories;
 
 namespace MovieAPI.Infraestructure.Shared;
 
@@ -9,6 +11,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration) {
         services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
+        services.AddTransient<IDirectorRepository, DirectorRepository>();
+        services.AddTransient<IMovieRepository, MovieRepository>();
 
         return services;
     }
