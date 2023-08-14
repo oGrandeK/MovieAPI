8978702b5a1b993b41e7e35ac94971de3e0e8bed
oGrandeK <kelvimkauam@gmail.com>
2023-08-14 19:05:12 -0300

    Finished DirectorController

--- MovieAPI.WebAPI/Controllers/DirectorController.cs
+++ MovieAPI.WebAPI/Controllers/DirectorController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MovieAPI.Domain.Entities;
 using MovieAPI.Domain.interfaces;
+using MovieAPI.Domain.Validation;
 
 namespace MovieAPI.WebAPI.Controllers;
 
@@ -37,11 +38,31 @@ public class DirectorController : ControllerBase
     }
 
     [HttpPost("v1/directors")]
-    public async Task<IActionResult> PostDirectorAsync([FromBody]Director directorData) {
+    public async Task<IActionResult> CreateDirectorAsync([FromBody]Director directorData) {
         var director = await _directorRepository.CreateDirectorAsync(directorData);
 
         return Ok(director);
     }
 
-    // TODO: Adicionar o Update e o Delete
+    [HttpPut("v1/directors/{id:int}")]
+    public async Task<IActionResult> UpdateDirectorAsync([FromQuery]int id, [FromBody]Director directorData) {
+        var director = await _directorRepository.GetDirectorMoviesByIdAsync(id) ?? null;
+        if(director is null) return NotFound(director);
+
+        director.UpdateName(directorData.Name);
+
+        await _directorRepository.UpdateDirectorAsync(director);
+
+        return Ok(director);
+    }
+
+    [HttpDelete("v1/directors/{id:int}")]
+    public async Task<IActionResult> DeleteDirectorAsync([FromQuery]int id) {
+        var director = await _directorRepository.GetDirectorMoviesByIdAsync(id) ?? null;
+        if(director is null) return NotFound(director);
+
+        await _directorRepository.DeleteDirectorAsync(director);
+
+        return Ok(director);
+    }
 }
