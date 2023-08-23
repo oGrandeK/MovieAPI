888806b60d4904338cae600e33ef6595619343b7
oGrandeK <kelvimkauam@gmail.com>
2023-08-22 22:38:17 -0300

    Configured Swagger to accept Genre as string

--- MovieAPI.WebAPI/Program.cs
+++ MovieAPI.WebAPI/Program.cs
@@ -1,4 +1,7 @@
 using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using MovieAPI.Domain.Enumerators;
 using MovieAPI.Domain.interfaces;
 using MovieAPI.Infraestructure.Repositories;
 using MovieAPI.Infraestructure.Shared;
@@ -11,7 +14,9 @@ builder.Services.AddServices(builder.Configuration);
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.MapType<GenreEnumerator>(() => new OpenApiSchema { Type = "string"});
+});
 
 var app = builder.Build();
 
