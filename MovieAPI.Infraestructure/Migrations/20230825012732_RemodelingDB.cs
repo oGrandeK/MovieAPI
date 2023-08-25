a07fb5c43334cf31bb1a77332ebbfb92e7c61c09
oGrandeK <kelvimkauam@gmail.com>
2023-08-24 23:08:21 -0300

    Droped and changed DataBase

---
+++ MovieAPI.Infraestructure/Migrations/20230825012732_RemodelingDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemodelingDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
