556823f33c5c3bb3a5885307a646ee5e82d0cb98
oGrandeK <kelvimkauam@gmail.com>
2023-09-15 19:27:48 -0300

    Added dependency injection of Director and Movie Interfaces, UseCases and Services. Refactored services scope for repositories and SMS, Email services

--- MovieAPI.Infraestructure.Shared/DependencyInjection.cs
+++ MovieAPI.Infraestructure.Shared/DependencyInjection.cs
@@ -2,7 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MovieAPI.Application.Interfaces;
+using MovieAPI.Application.Interfaces.Services;
+using MovieAPI.Application.Interfaces.UseCases.DirectorUseCases;
+using MovieAPI.Application.Interfaces.UseCases.MovieUseCases;
 using MovieAPI.Application.Services;
+using MovieAPI.Application.UseCases.DirectorUseCases;
+using MovieAPI.Application.UseCases.MovieUsecases;
 using MovieAPI.Domain.interfaces;
 using MovieAPI.Infraestructure.Context;
 using MovieAPI.Infraestructure.Repositories;
@@ -14,11 +19,28 @@ public static class DependencyInjection
     public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration) {
         services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
 
-        services.AddTransient<IDirectorRepository, DirectorRepository>();
-        services.AddTransient<IMovieRepository, MovieRepository>();
+        services.AddScoped<IDirectorRepository, DirectorRepository>();
+        services.AddScoped<IMovieRepository, MovieRepository>();
 
-        services.AddSingleton<IEmailService, SendGridEmailService>();
-        services.AddSingleton<ISmsService, TwilioSmsService>();
+        services.AddTransient<IEmailService, SendGridEmailService>();
+        services.AddTransient<ISmsService, TwilioSmsService>();
+
+        services.AddScoped<IDirectorService, DirectorService>();
+        services.AddScoped<IMovieService, MovieService>();
+
+        services.AddScoped<IAddDirectorUseCase, AddDirectorUseCase>();
+        services.AddScoped<IDeleteDirectorUseCase, DeleteDirectorUseCase>();
+        services.AddScoped<IListAllDirectorsUseCase, ListAllDirectorsUseCase>();
+        services.AddScoped<IListDirectorByIdUseCase, ListDirectorByIdUseCase>();
+        services.AddScoped<IListDirectorsByNameUseCase, ListDirectorsByNameUseCase>();
+        services.AddScoped<IUpdateDirectorUseCase, UpdateDirectorUseCase>();
+        services.AddScoped<IAddMovieUseCase, AddMovieUseCase>();
+        services.AddScoped<IDeleteMovieUseCase, DeleteMovieUseCase>();
+        services.AddScoped<IListAllMoviesUseCase, ListAllMoviesUseCase>();
+        services.AddScoped<IListMovieByIdUseCase, ListMovieByIdUseCase>();
+        services.AddScoped<IListMoviesByGenreUseCase, ListMoviesByGenreUseCase>();
+        services.AddScoped<IListMoviesByTitleUseCase, ListMoviesByTitleUseCase>();
+        services.AddScoped<IUpdateMovieUseCase, UpdateMovieUseCase>();
 
         return services;
     }
