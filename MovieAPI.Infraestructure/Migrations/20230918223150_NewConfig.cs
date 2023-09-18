3542f491e783152b688f9563651f36c912ae4376
oGrandeK <kelvimkauam@gmail.com>
2023-09-18 19:38:04 -0300

    New migration

---
+++ MovieAPI.Infraestructure/Migrations/20230918223150_NewConfig.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Infraestructure.Migrations
+{
+    public partial class NewConfig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDate",
+                table: "Movies",
+                newName: "Release date");
+
+            migrationBuilder.RenameColumn(
+                name: "DurationInMinutes",
+                table: "Movies",
+                newName: "Duration in minutes");
+
+            migrationBuilder.RenameColumn(
+                name: "Title_MovieTitle",
+                table: "Movies",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "Name_LastName",
+                table: "Directors",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name_FirstName",
+                table: "Directors",
+                newName: "FirstName");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "Movies",
+                type: "FLOAT",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movies",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Movies",
+                type: "NVARCHAR(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Release date",
+                table: "Movies",
+                type: "DATE",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<short>(
+                name: "Duration in minutes",
+                table: "Movies",
+                type: "SMALLINT",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "NVARCHAR(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Directors",
+                type: "NVARCHAR(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Directors",
+                type: "NVARCHAR(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Release date",
+                table: "Movies",
+                newName: "ReleaseDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Duration in minutes",
+                table: "Movies",
+                newName: "DurationInMinutes");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Movies",
+                newName: "Title_MovieTitle");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Directors",
+                newName: "Name_LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Directors",
+                newName: "Name_FirstName");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "Movies",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "FLOAT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Movies",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "DATE",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "DurationInMinutes",
+                table: "Movies",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(short),
+                oldType: "SMALLINT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title_MovieTitle",
+                table: "Movies",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name_LastName",
+                table: "Directors",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name_FirstName",
+                table: "Directors",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
