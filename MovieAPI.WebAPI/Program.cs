29edbc2d25f06b839535896c7fbb91fe530c3f19
oGrandeK <kelvimkauam@gmail.com>
2023-09-18 22:50:11 -0300

    Refactored Program.cs

--- MovieAPI.WebAPI/Program.cs
+++ MovieAPI.WebAPI/Program.cs
@@ -16,9 +16,7 @@ builder.Services.AddServices(builder.Configuration);
 builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => {
-    options.MapType<GenreEnumerator>(() => new OpenApiSchema { Type = "string" });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
