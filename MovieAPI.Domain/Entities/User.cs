a1cff43143596c18b8b5708f981032c47a4ce47a
oGrandeK <kelvimkauam@gmail.com>
2023-09-21 20:06:08 -0300

    removed PasswordService

--- MovieAPI.Domain/Entities/User.cs
+++ MovieAPI.Domain/Entities/User.cs
@@ -1,28 +1,22 @@
-using MovieAPI.Domain.interfaces;
 using MovieAPI.Domain.ValueObjects;
 
 namespace MovieAPI.Domain.Entities;
 
 public class User : Entity
 {
-    private readonly IPasswordService _passwordService;
-
     public Name Name { get; set; }
     public Email Email { get; set; }
     public Password Password { get; set; }
 
     private User()
     {
-        
+
     }
 
-    public User(IPasswordService passwordService ,string name, string email, string password)
+    public User(string name, string email, string hashedPassword)
     {
-        _passwordService = passwordService;
-
         Name = name;
         Email = email;
-
-        Password = _passwordService.Hash(password);
+        Password = hashedPassword;
     }
 }
