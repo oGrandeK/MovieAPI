03b41b3ffd1d79d58c389d55fb5b7b4bec17bc10
oGrandeK <kelvimkauam@gmail.com>
2023-09-21 20:07:16 -0300

    Solved bug in Email.cs / refactored Password.cs

--- MovieAPI.Domain/ValueObjects/Email.cs
+++ MovieAPI.Domain/ValueObjects/Email.cs
@@ -6,24 +6,24 @@ public class Email : ValueObject
 {
     const string Pattern = @"\w+@\w+\.\w+";
     public string Address { get; private set; }
-    public Verification Verification { get; set; }
+    public Verification Verification { get; set; } = new Verification();
 
     private static Regex EmailRegex() => new Regex(Pattern);
 
     private Email()
     {
-        
+
     }
 
     public Email(string address)
     {
-        if(string.IsNullOrEmpty(address)) throw new Exception("E-mail não pode ser nulo ou vazio");
+        if (string.IsNullOrEmpty(address)) throw new Exception("E-mail não pode ser nulo ou vazio");
 
         Address = address.Trim().ToLower();
 
-        if(Address.Length < 5) throw new Exception("E-mail deve conter mais que 5 caracteres");
+        if (Address.Length < 5) throw new Exception("E-mail deve conter mais que 5 caracteres");
 
-        if(!EmailRegex().IsMatch(Address)) throw new Exception("E-mail inserido não está com formato válido");
+        if (!EmailRegex().IsMatch(Address)) throw new Exception("E-mail inserido não está com formato válido");
     }
 
     public static implicit operator string(Email email) => email.ToString();
--- MovieAPI.Domain/ValueObjects/Password.cs
+++ MovieAPI.Domain/ValueObjects/Password.cs
@@ -6,7 +6,7 @@ public class Password : ValueObject
 
     private Password()
     {
-        
+
     }
 
     public Password(string hash)
