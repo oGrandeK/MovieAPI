f28d7246c31bbf0a19e3737e9822d365e6be0637
oGrandeK <kelvimkauam@gmail.com>
2023-10-05 19:38:02 -0300

    Removed MoviesDTO from Presentation Layer

--- MovieAPI.WebAPI/DTOs/Movies/CreateMovieDTO.cs
+++
@@ -1,3 +0,0 @@
-namespace MovieAPI.WebAPI.DTOs.Movies;
-
-public record CreateMovieDTO(string Title, string? Description, string? Genre, short? DurationInMinutes, string? ReleaseDate, double? Rating, int Director);
--- MovieAPI.WebAPI/DTOs/Movies/GetMoviesDTO.cs
+++
@@ -1,3 +0,0 @@
-namespace MovieAPI.WebAPI.DTOs.Movies;
-
-public record GetMoviesDTO(int Id, string Title, string? Description, string? Genre, short? DurationInMinutes, string? ReleaseDate, double? Rating, string Director);
