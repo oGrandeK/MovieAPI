e2063a6be79539eb4aa47ef40198a7281491d725
oGrandeK <kelvimkauam@gmail.com>
2023-10-05 20:01:44 -0300

    Removed unnecessary using

--- MovieAPI.Application/DTOs/Movies/GetMoviesDTO.cs
+++ MovieAPI.Application/DTOs/Movies/GetMoviesDTO.cs
@@ -1,5 +1,3 @@
-using MovieAPI.Domain.Entities;
-
 namespace MovieAPI.Application.DTOs.Movies;
 
 public record GetMoviesDTO(int Id, string Title, string? Description, string? Genre, short? DurationInMinutes, string? ReleaseDate, double? Rating, string DirectorName);
