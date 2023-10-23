6bcc05491e0fb36c4b2b62319d0c5e15c899ba0a
oGrandeK <kelvimkauam@gmail.com>
2023-10-23 20:47:08 -0300

    Created documentation for DomainExceptionValidation.cs

--- MovieAPI.Domain/Validation/DomainExceptionValidation.cs
+++ MovieAPI.Domain/Validation/DomainExceptionValidation.cs
@@ -1,13 +1,27 @@
 namespace MovieAPI.Domain.Validation;
 
+/// <summary>
+/// Exceção personalizada para representar erros de validação no domínio.
+/// </summary>
 public class DomainExceptionValidation : Exception
 {
+    /// <summary>
+    /// Inicializa uma nova instância da classe <see cref="DomainExceptionValidation"/>
+    /// </summary>
+    /// <param name="error">A mensagem de erro associada à exeção.</param>
     public DomainExceptionValidation(string error) : base(error)
     {
-        
+
     }
 
-    public static void HasError(bool HasError, string error) {
-        if(HasError) throw new DomainExceptionValidation(error);
+    /// <summary>
+    /// Lança uma exceção <see cref="DomainExceptionValidation"/> se a condição for verdadeira.
+    /// </summary>
+    /// <param name="hasError">A condição do erro.</param>
+    /// <param name="error">A mensagem de erro associada à exceção.</param>
+    /// <exception cref="DomainExceptionValidation">Exceção lançada se a condição do erro for verdadeira.</exception>
+    public static void HasError(bool hasError, string error)
+    {
+        if (hasError) throw new DomainExceptionValidation(error);
     }
 }
