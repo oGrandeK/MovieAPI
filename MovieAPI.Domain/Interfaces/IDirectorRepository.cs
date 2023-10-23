ba67a538a188985fa3115088338d5477de7ae38c
oGrandeK <kelvimkauam@gmail.com>
2023-10-23 20:06:52 -0300

    Refactored IDirectorRepository.cs

--- MovieAPI.Domain/Interfaces/IDirectorRepository.cs
+++ MovieAPI.Domain/Interfaces/IDirectorRepository.cs
@@ -25,6 +25,7 @@ public interface IDirectorRepository
     /// Obtém todos os diretores com informações sobre seus filmes pelo nome.
     /// </summary>
     /// <param name="directorName">O nome do diretor a ser pesquisado.</param>
+    /// <returns>Uma coleção de diretores com detalhes sobre seus filmes.</returns>
     Task<IEnumerable<Director>> GetDirectorsMoviesByNameAsync(string directorName);
 
     /// <summary>
@@ -37,14 +38,14 @@ public interface IDirectorRepository
     /// <summary>
     /// Atualiza as informações do diretor.
     /// </summary>
-    /// <param name="director">O diretor com as informações atualizadas.</param>
+    /// <param name="director">O diretor com as informações atualizadas. Consulte <see cref="Director"/> para mais detalhes sobre diretor.</param>
     /// <returns>O diretor atualizado com detalhes de seus filmes.</returns>
     Task<Director> UpdateDirectorAsync(Director director);
 
     /// <summary>
     /// Deleta o diretor.
     /// </summary>
-    /// <param name="director">O diretor a ser deletado.</param>
+    /// <param name="director">O diretor a ser deletado. Consulte <see cref="Director"/> para mais detalhes sobre diretor.</param>
     /// <returns>O diretor deletado.</returns>
     Task<Director> DeleteDirectorAsync(Director director);
 }
