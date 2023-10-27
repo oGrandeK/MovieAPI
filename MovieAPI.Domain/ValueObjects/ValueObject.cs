50c49e3d1714f5c3667292fe0f06f0459ea586c6
oGrandeK <kelvimkauam@gmail.com>
2023-10-27 20:17:59 -0300

    Created documentation for ValueObject.cs

--- MovieAPI.Domain/ValueObjects/ValueObject.cs
+++ MovieAPI.Domain/ValueObjects/ValueObject.cs
@@ -1,6 +1,9 @@
 namespace MovieAPI.Domain.ValueObjects;
 
+/// <summary>
+/// Classe abstrata que serve como base para objetos de valor.
+/// </summary>
 public abstract class ValueObject
 {
-    
+
 }
