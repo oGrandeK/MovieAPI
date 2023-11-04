7c66f07ce7e0b3681600a0a95833269b418abe9d
oGrandeK <kelvimkauam@gmail.com>
2023-11-03 21:21:52 -0300

    Created documentation for DirectorConfiguration.cs

--- MovieAPI.Infraestructure/EntitiesConfiguration/DirectorConfiguration.cs
+++ MovieAPI.Infraestructure/EntitiesConfiguration/DirectorConfiguration.cs
@@ -4,8 +4,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Infraestructure.EntitiesConfiguration;
 
+/// <summary>
+/// Configuração da entidade <see cref="Director"/> para mapeamento no banco de dados.
+/// </summary>
 public class DirectorConfiguration : IEntityTypeConfiguration<Director>
 {
+    /// <summary>
+    /// Configura o mapeamento da entidade <see cref="Director"/>.
+    /// </summary>
+    /// <param name="builder">O construtor da entidade.</param>
     public void Configure(EntityTypeBuilder<Director> builder)
     {
         builder.HasKey(x => x.Id);
