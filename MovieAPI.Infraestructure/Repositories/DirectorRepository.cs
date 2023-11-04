da5976a5cb24fa8c5013057c4283b5f21a6b3320
oGrandeK <kelvimkauam@gmail.com>
2023-11-03 22:07:59 -0300

    Created documentation for DirectorRepository.cs

--- MovieAPI.Infraestructure/Repositories/DirectorRepository.cs
+++ MovieAPI.Infraestructure/Repositories/DirectorRepository.cs
@@ -6,19 +6,29 @@ using MovieAPI.Infraestructure.Context;
 
 namespace MovieAPI.Infraestructure.Repositories;
 
+/// <summary>
+/// Implementação do repositório para operações relacionadas a diretores no banco de dados.
+/// </summary>
 public class DirectorRepository : IDirectorRepository
 {
     private readonly ApplicationContext _context;
 
+    /// <summary>
+    /// Inicializa uma nova instância da classe <see cref="DirectorRepository"/>.
+    /// </summary>
+    /// <param name="context">O contexto do banco de dados.</param>
     public DirectorRepository(ApplicationContext context)
     {
         _context = context;
     }
 
+    /// <inheritdoc/>
     public async Task<IEnumerable<Director>> GetDirectorsMoviesAsync() => await _context.Directors.Include(x => x.Movies).AsNoTracking().ToListAsync();
 
+    /// <inheritdoc/>
     public async Task<Director> GetDirectorMoviesByIdAsync(int id) => await _context.Directors.Include(x => x.Movies).AsNoTracking().SingleOrDefaultAsync(x => x.Id == id) ?? throw new DomainExceptionValidation($"Director cannot be found by id - {id}");
 
+    /// <inheritdoc/>
     public async Task<IEnumerable<Director>> GetDirectorsMoviesByNameAsync(string directorName)
     {
         var names = directorName.Split(" ", StringSplitOptions.RemoveEmptyEntries);
@@ -36,6 +46,7 @@ public class DirectorRepository : IDirectorRepository
         return directors;
     }
 
+    /// <inheritdoc/>
     public async Task<Director> CreateDirectorAsync(Director director)
     {
         var directorExist = await DirectorExist(director.Name);
@@ -51,6 +62,7 @@ public class DirectorRepository : IDirectorRepository
         throw new DomainExceptionValidation("Director already exists");
     }
 
+    /// <inheritdoc/>
     public async Task<Director> UpdateDirectorAsync(Director director)
     {
         _context.Directors.Update(director);
@@ -59,6 +71,7 @@ public class DirectorRepository : IDirectorRepository
         return director;
     }
 
+    /// <inheritdoc/>
     public async Task<Director> DeleteDirectorAsync(Director director)
     {
         _context.Directors.Remove(director);
@@ -67,6 +80,11 @@ public class DirectorRepository : IDirectorRepository
         return director;
     }
 
+    /// <summary>
+    /// Verifica se o diretor já existe no banco de dados.
+    /// </summary>
+    /// <param name="directorName">O nome do diretor a ser verificado.</param>
+    /// <returns>True se o diretor já existir no banco de dados; caso contrário False.</returns>
     public async Task<bool> DirectorExist(string directorName)
     {
         try
