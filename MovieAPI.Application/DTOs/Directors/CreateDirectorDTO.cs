d312b7cd67f8fd6fb2848bb4828798532be9d2a0
oGrandeK <kelvimkauam@gmail.com>
2023-11-14 18:22:48 -0300

    Created documentation for CreateDirectorDTO.cs

--- MovieAPI.Application/DTOs/Directors/CreateDirectorDTO.cs
+++ MovieAPI.Application/DTOs/Directors/CreateDirectorDTO.cs
@@ -2,4 +2,8 @@ using MovieAPI.Domain.ValueObjects;
 
 namespace MovieAPI.Application.DTOs.Directors;
 
+/// <summary>
+/// DTO para criação de um Diretor.
+/// </summary>
+/// <param name="Name">Obtém ou define o nome do diretor. veja <see cref="Domain.ValueObjects.Name"/> para obter mais detalhes.</param>
 public record CreateDirectorDTO(Name Name);
