268f830abdfba7e38b7476c507d4ac898c13d1e3
oGrandeK <kelvimkauam@gmail.com>
2023-11-14 18:34:14 -0300

    Created documentation for GetMoviesDTO.cs | Minor changes in CreateMovieDTO.cs

--- MovieAPI.Application/DTOs/Movies/CreateMovieDTO.cs
+++ MovieAPI.Application/DTOs/Movies/CreateMovieDTO.cs
@@ -8,6 +8,6 @@ namespace MovieAPI.Application.DTOs.Movies;
 /// <param name="Genre">Define o gênero do filme.</param>
 /// <param name="DurationInMinutes">Define a duração do filme em minutos.</param>
 /// <param name="ReleaseDate">Define a data de lançamento do filme.</param>
-/// <param name="Rating">Define a nota do filme.</param>
+/// <param name="Rating">Define a classificação do filme.</param>
 /// <param name="DirectorId">Define o ID do diretor associado ao filme.</param>
 public record CreateMovieDTO(string Title, string? Description, string? Genre, short? DurationInMinutes, string? ReleaseDate, double? Rating, int DirectorId);
--- MovieAPI.Application/DTOs/Movies/GetMoviesDTO.cs
+++ MovieAPI.Application/DTOs/Movies/GetMoviesDTO.cs
@@ -1,3 +1,14 @@
 namespace MovieAPI.Application.DTOs.Movies;
 
+/// <summary>
+/// DTO para obter informações sobre filmes.
+/// </summary>
+/// <param name="Id">Obtém o ID do filme.</param>
+/// <param name="Title">Obtém o título do filme.</param>
+/// <param name="Description">Obtém a descrição do filme.</param>
+/// <param name="Genre">Obtém o gênero do filme.</param>
+/// <param name="DurationInMinutes">Obtém a duração do filme em minutos.</param>
+/// <param name="ReleaseDate">Obtém a data de lançamento do filme.</param>
+/// <param name="Rating">Obtém a classificação do filme.</param>
+/// <param name="DirectorName">Obtém o nome do diretor associado ao filme.</param>
 public record GetMoviesDTO(int Id, string Title, string? Description, string? Genre, short? DurationInMinutes, string? ReleaseDate, double? Rating, string DirectorName);
