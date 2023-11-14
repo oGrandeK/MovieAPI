4d3c07e57fb0f1cd21deea55ab7aa0fc94f7ac1e
oGrandeK <kelvimkauam@gmail.com>
2023-11-14 18:27:04 -0300

    Created documentation for GetDirectorsDTO.cs

--- MovieAPI.Application/DTOs/Directors/GetDirectorsDTO.cs
+++ MovieAPI.Application/DTOs/Directors/GetDirectorsDTO.cs
@@ -1,5 +1,17 @@
 namespace MovieAPI.Application.DTOs.Directors;
 
+/// <summary>
+/// DTO para obter informações sobre diretores.
+/// </summary>
+/// <param name="Id">Obtém o ID do diretor.</param>
+/// <param name="Name">Obtém o nome do diretor.</param>
+/// <param name="Movies">Obtém a lista de filmes associados ao diretor.</param>
 public record GetDirectorsDTO(int Id, string Name, List<MovieDTO>? Movies);
 
+/// <summary>
+/// DTO para obter informações sobre filmes associados a diretores.
+/// </summary>
+/// <param name="Id">Obtém o ID do filme.</param>
+/// <param name="Title">Obtém o título do filme.</param>
+/// <param name="ReleaseDate">Obtém a data de lançamento do filme.</param>
 public record MovieDTO(int Id, string Title, string ReleaseDate);
