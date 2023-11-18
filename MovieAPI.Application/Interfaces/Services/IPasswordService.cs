00f13e9c70f53446c172d5f8259f68cb3b720298
oGrandeK <kelvimkauam@gmail.com>
2023-11-17 21:01:40 -0300

    Created documentation for IPasswordService.cs

--- MovieAPI.Application/Interfaces/Services/IPasswordService.cs
+++ MovieAPI.Application/Interfaces/Services/IPasswordService.cs
@@ -1,8 +1,22 @@
 namespace MovieAPI.Application.Interfaces.Services;
 
+/// <summary>
+/// Interface para serviços relacionados as senhas.
+/// </summary>
 public interface IPasswordService
 {
+    /// <summary>
+    /// Gera um novo hash.
+    /// </summary>
+    /// <param name="password">A cadeia de caracteres a ser hasheada.</param>
+    /// <returns>Uma cadeia de caracteres hasheadas.</returns>
     public string Hash(string password);
 
+    /// <summary>
+    /// Verifica se a senha tem o mesmo hash.
+    /// </summary>
+    /// <param name="hash">A senha a ser verificada.</param>
+    /// <param name="password">O hash a ser comparado.</param>
+    /// <returns>Um boolenado indicando se a verificação foi bem-sucedida. </returns>
     public bool Verify(string hash, string password);
 }
