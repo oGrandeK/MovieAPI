f99c8bc0408f4442e91d8faac9d0a4f0c50dce19
oGrandeK <kelvimkauam@gmail.com>
2023-11-17 21:09:13 -0300

    Created documentation for ITokenService.cs

--- MovieAPI.Application/Interfaces/Services/ITokenService.cs
+++ MovieAPI.Application/Interfaces/Services/ITokenService.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.Services;
 
+/// <summary>
+/// Interface para serviços relacionados a geração de tokens jwt.
+/// </summary>
 public interface ITokenService
 {
+    /// <summary>
+    /// Gera um novo token.
+    /// </summary>
+    /// <param name="user">O <see cref="User"/> a ser usado como base pra criação do token.</param>
+    /// <returns>Uma cadeia de caracteres contendo o token gerado.</returns>
     public string GenerateToken(User user);
 }
