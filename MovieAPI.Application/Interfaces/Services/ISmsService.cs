e8954a53ae300bf10613fb1af4c34788ed745f85
oGrandeK <kelvimkauam@gmail.com>
2023-11-17 21:06:21 -0300

    Created documentation for ISmsService.cs

--- MovieAPI.Application/Interfaces/Services/ISmsService.cs
+++ MovieAPI.Application/Interfaces/Services/ISmsService.cs
@@ -1,6 +1,15 @@
 namespace MovieAPI.Application.Interfaces.Services;
 
+/// <summary>
+/// Interface para serviços relacionados ao envio de SMS.
+/// </summary>
 public interface ISmsService
 {
+    /// <summary>
+    /// Envia um novo SMS.
+    /// </summary>
+    /// <param name="from">O número de telefone que enviará o SMS.</param>
+    /// <param name="to">O número de telefone que receberá o SMS.</param>
+    /// <param name="body">A mensagem que será enviada pelo SMS.</param>
     void SendSms(string from, string to, string body);
 }
