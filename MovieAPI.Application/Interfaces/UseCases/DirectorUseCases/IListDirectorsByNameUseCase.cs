a389461cbdeb4b9f96c0c48347a068b57e50c4bf
oGrandeK <kelvimkauam@gmail.com>
2023-11-20 20:45:03 -0300

    Created documentation for IListDirectorByNameUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IListDirectorsByNameUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IListDirectorsByNameUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.DirectorUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de diretores pelo nome fornecido.
+/// </summary>
 public interface IListDirectorsByNameUseCase
 {
+    /// <summary>
+    /// Lista todos os <see cref="Director"/> com base no nome fornecido.
+    /// </summary>
+    /// <param name="name">O nome do diretor a ser procurado.</param>
+    /// <returns>Uma coleção de <see cref="Director"/> com base no nome fornecido.</returns>
     public Task<IEnumerable<Director>> Execute(string name);
 }
