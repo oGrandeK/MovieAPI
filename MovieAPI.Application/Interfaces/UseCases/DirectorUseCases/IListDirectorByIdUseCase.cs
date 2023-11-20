8e9cbb4f43fb4327fe022e3feb41c0c5f15cc521
oGrandeK <kelvimkauam@gmail.com>
2023-11-20 20:38:40 -0300

    Created documentation for IListDirectorByIdUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IListDirectorByIdUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IListDirectorByIdUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.DirectorUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de um diretor pelo ID fornecido.
+/// </summary>
 public interface IListDirectorByIdUseCase
 {
+    /// <summary>
+    /// Lista um diretor pelo ID fornecido.
+    /// </summary>
+    /// <param name="id">O ID a ser procurado.</param>
+    /// <returns>Retorna um único <see cref="Director"/> com base no ID fornecido.</returns>
     public Task<Director> Execute(int id);
 }
