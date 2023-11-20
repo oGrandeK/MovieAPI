91ac928677c2f6b06af2c9c7772e31bca3b461af
oGrandeK <kelvimkauam@gmail.com>
2023-11-20 20:25:35 -0300

    Created documentation for IAddDirectorUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IAddDirectorUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IAddDirectorUseCase.cs
@@ -2,7 +2,14 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.DirectorUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a criação de um novo Diretor.
+/// </summary>
 public interface IAddDirectorUseCase
 {
+    /// <summary>
+    /// Cria um novo <see cref="Director"/> com base nas informações passadas.
+    /// </summary>
+    /// <returns>Uma tarefa que representa a operação assíncrona.</returns>
     public Task Execute(Director director);
 }
