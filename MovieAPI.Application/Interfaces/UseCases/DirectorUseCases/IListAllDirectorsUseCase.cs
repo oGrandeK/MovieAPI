28e0c534c10213f121d51a59eafff7639a3d0e08
oGrandeK <kelvimkauam@gmail.com>
2023-11-20 20:35:59 -0300

    Created documentation for IListAllDirectorsUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IListAllDirectorsUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IListAllDirectorsUseCase.cs
@@ -2,7 +2,14 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.DirectorUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem dos diretores.
+/// </summary>
 public interface IListAllDirectorsUseCase
 {
+    /// <summary>
+    /// Retorna os diretores.
+    /// </summary>
+    /// <returns>Uma coleção de <see cref="Director"/>.</returns>
     public Task<IEnumerable<Director>> Execute();
 }
