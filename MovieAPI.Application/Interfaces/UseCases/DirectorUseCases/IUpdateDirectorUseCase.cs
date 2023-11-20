ba991aa9ddd761b03410a3893f61ccba4510fbb9
oGrandeK <kelvimkauam@gmail.com>
2023-11-20 20:50:58 -0300

    Created documentation for IUpdateDirectorUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IUpdateDirectorUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/DirectorUseCases/IUpdateDirectorUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.DirectorUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a atualização de diretores.
+/// </summary>
 public interface IUpdateDirectorUseCase
 {
+    /// <summary>
+    /// Atualiza as informações de um diretor pelo ID fornecido.
+    /// </summary>
+    /// <param name="id">O ID do <see cref="Director"/> a ser procurado.</param>
+    /// <param name="directorData">As novas informações do diretor.</param>
     public Task Execute(int id, Director directorData);
 }
