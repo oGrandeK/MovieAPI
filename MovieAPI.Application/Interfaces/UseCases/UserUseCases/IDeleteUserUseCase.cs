dec98bccb4eb556a2ed03efe0f4b78771712aa6d
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 20:56:25 -0300

    Created documentation for IDeleteUserUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IDeleteUserUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IDeleteUserUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a exclusão de um usuário.
+/// <summary>
 public interface IDeleteUserUseCase
 {
+    /// <summary>
+    /// Excluí um usuário com base no ID fornecido.
+    /// </summary>
+    /// <param name="id">O ID do usuário a ser procurado.</param>
+    /// <returns>O <see cref="User"/> excluído.</returns>
     public Task<User> Execute(int id);
 }
