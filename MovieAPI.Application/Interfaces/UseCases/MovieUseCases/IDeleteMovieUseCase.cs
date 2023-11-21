9af9351f07c496d4ac5e365b8b63b6b6466fbd6e
oGrandeK <kelvimkauam@gmail.com>
2023-11-20 21:01:55 -0300

    Created documentation for IDeleteMovieUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IDeleteMovieUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IDeleteMovieUseCase.cs
@@ -1,6 +1,15 @@
+using MovieAPI.Domain.Entities;
+
 namespace MovieAPI.Application.Interfaces.UseCases.MovieUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a exclusão de um filme.
+/// </summary>
 public interface IDeleteMovieUseCase
 {
+    /// <summary>
+    /// Exclui um <see cref="Movie"/> com base no ID fornecido.
+    /// </summary>
+    /// <param name="id">O ID do <see cref="Movie"/> a ser excluído.</param>
     public Task Execute(int id);
 }
