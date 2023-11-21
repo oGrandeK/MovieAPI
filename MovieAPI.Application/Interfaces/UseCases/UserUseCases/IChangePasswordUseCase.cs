27c4e94e75f3f732498fd48e9ffae533de992309
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 20:52:32 -0300

    Created documentation for IChangePasswordUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IChangePasswordUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IChangePasswordUseCase.cs
@@ -2,7 +2,17 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a alteração de senha do usuário.
+/// </summary>
 public interface IChangePasswordUseCase
 {
+    /// <summary>
+    /// Altera a senha do usuário.
+    /// </summary>
+    /// <param name="user">O usuário a ter a senha alterada.</param>
+    /// <param name="oldPassword">A antiga senha do usuário.</param>
+    /// <param name="newPassword">A nova senha do usuário.</param>
+    /// <returns>Um <see cref="User"/> com a senha alterada.</returns>
     Task<User> Change(User user, string oldPassword, string newPassword);
 }
