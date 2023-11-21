7badf0f2e37cc88e2a3864d91f1839e9ba4fcafe
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 20:42:18 -0300

    Created documentation for IUpdateMovieUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IUpdateMovieUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IUpdateMovieUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.MovieUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a atualização de filmes.
+/// </summary>
 public interface IUpdateMovieUseCase
 {
+    /// <summary>
+    /// Atualiza o filme com base no ID fornecido.
+    /// </summary>
+    /// <param name="id">O ID do filme a ser atualizado.</param>
+    /// <param name="movieData">As novas informações do filme.</param>
     public Task Execute(int id, Movie movieData);
 }
