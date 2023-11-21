2a35db0a468170f9986333fd090dfcc124223429
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 20:31:29 -0300

    Created documentation for IListMovieByGenreUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListMoviesByGenreUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListMoviesByGenreUseCase.cs
@@ -3,7 +3,15 @@ using MovieAPI.Domain.Enumerators;
 
 namespace MovieAPI.Application.Interfaces.UseCases.MovieUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de filmes pelo gênero.
+/// </summary>
 public interface IListMoviesByGenreUseCase
 {
+    /// <summary>
+    /// Retorna os filmes pelo gênero fornecido.
+    /// </summary>
+    /// <param name="genre">O gênero dos filmes a serem pesquisados.</param>
+    /// <returns>Uma coleção de <see cref="Movie"/> com base no gênero fornecido.</returns>
     public Task<IEnumerable<Movie>> Execute(GenreEnumerator genre);
 }
