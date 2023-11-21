495ec7ba29731fca2fd91baa7cab495d02638825
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 20:39:13 -0300

    Created documentation for IListMovieByTitleUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListMoviesByTitleUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListMoviesByTitleUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.MovieUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de filmes pelo título.
+/// </summary>
 public interface IListMoviesByTitleUseCase
 {
-    public Task<IEnumerable<Movie>> Execute(string name);
+    /// <summary>
+    /// Retorna os filmes com base no título fornecido.
+    /// </summary>
+    /// <param name="title">O título do filme a ser pesquisado.</param>
+    /// <returns>Uma coleção de <see cref="Movie"/> com base no título fornecido.</returns>
+    public Task<IEnumerable<Movie>> Execute(string title);
 }
