39b4db812ca66783bf3b8ba41c7265d20174b465
oGrandeK <kelvimkauam@gmail.com>
2023-11-20 21:05:08 -0300

    Created documentation for IListAllMoviesUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListAllMoviesUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListAllMoviesUseCase.cs
@@ -2,7 +2,14 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.MovieUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de filmes.
+/// </summary>
 public interface IListAllMoviesUseCase
 {
+    /// <summary>
+    /// Retorna os filmes.
+    /// </summary>
+    /// <returns>Uma coleção de <see cref="Movie"/>.</returns>
     public Task<IEnumerable<Movie>> Execute();
 }
