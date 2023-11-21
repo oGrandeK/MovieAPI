4027519879ec4f73c6962b2341654c090acbc217
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 20:25:13 -0300

    Created documentation for IListMovieByIdUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListMovieByIdUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/MovieUseCases/IListMovieByIdUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.MovieUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de filme pelo ID.
+/// </summary>
 public interface IListMovieByIdUseCase
 {
+    /// <summary>
+    /// Retorna um único filme pelo ID fornecido.
+    /// </summary>
+    /// <param name="id">O ID do filme a ser excluído.</param>
+    /// <returns>Um único <see cref="Movie"/>.</returns>
     public Task<Movie> Execute(int id);
 }
