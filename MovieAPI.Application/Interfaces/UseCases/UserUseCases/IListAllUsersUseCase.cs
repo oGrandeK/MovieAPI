0c6ae6f37621848b9a5fdf7df1d63de3b43aa0fa
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 20:59:16 -0300

    Created documentation for IListAllUsersUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListAllUsersUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListAllUsersUseCase.cs
@@ -2,7 +2,14 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de usuários.
+/// </summary>
 public interface IListAllUsersUseCase
 {
+    /// <summary>
+    /// Retorna todos os usuários.
+    /// </summary>
+    /// <returns>Uma coleção de <see cref="User"/>.</returns>
     public Task<IEnumerable<User>> Execute();
 }
