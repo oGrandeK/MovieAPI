bd0918cce449b379a8b81f5814d1036cbc4aa54b
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 21:05:34 -0300

    Created documentation for IListUserByIdUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListUserByIdUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListUserByIdUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de um usuário pelo ID fornecido.
+/// </summary>
 public interface IListUserByIdUseCase
 {
+    /// <summary>
+    /// Retorna um único usuário com base no ID fornecido.
+    /// </summary>
+    /// <param name="id">O ID do usuário a ser pesquisado.</param>
+    /// <returns>Um único <see cref="User"/> com base no ID fornecido.</returns>
     public Task<User> Execute(int id);
 }
