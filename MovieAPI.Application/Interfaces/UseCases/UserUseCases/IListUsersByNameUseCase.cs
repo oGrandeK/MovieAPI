ceebba161a02ce94f412e0385d8cb5c3cec745d6
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 21:25:01 -0300

    Created documentation for IListUsersByNameUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListUsersByNameUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListUsersByNameUseCase.cs
@@ -2,7 +2,15 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a listagem de usuários pelo nome fornecido.
+/// </summary>
 public interface IListUsersByNameUseCase
 {
+    /// <summary>
+    /// Retorna lista de usuários com base no nome fornecido.
+    /// </summary>
+    /// <param name="fullname">O nome completo separado por espaço do usuário a ser procurado.</param>
+    /// <returns>Uma coleção de <see cref="User"/> com base no nome completo fornecido.</returns>
     public Task<IEnumerable<User>> Execute(string fullname);
 }
