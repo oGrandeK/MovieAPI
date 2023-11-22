f2c59161bbf47c91014d8c4c7a3c2a38af2fa18d
oGrandeK <kelvimkauam@gmail.com>
2023-11-21 21:02:02 -0300

    IListUserByEmailUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListUserByEmailUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IListUserByEmailUseCase.cs
@@ -1,8 +1,15 @@
 using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
-
+/// <summary>
+/// Interface para serviços relacionados a listagem do usuário pelo e-mail.
+/// </summary>
 public interface IListUserByEmailUseCase
 {
+    /// <summary>
+    /// Retorna um único usuário com base no e-mail fornecido.
+    /// </summary>
+    /// <param name="email">O e-mail do usuário a ser procurado.</param>
+    /// <returns>Um <see cref="User"/> com base no e-mail fornecido.</returns>
     public Task<User> Execute(string email);
 }
