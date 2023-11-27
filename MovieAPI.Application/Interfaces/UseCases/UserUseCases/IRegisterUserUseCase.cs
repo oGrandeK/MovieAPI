69752821ea020e4841361f39b636825c7f06d170
oGrandeK <kelvimkauam@gmail.com>
2023-11-27 20:04:03 -0300

    Created documentation for IRegisterUserUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IRegisterUserUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IRegisterUserUseCase.cs
@@ -2,7 +2,18 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados ao registro de usuário.
+/// </summary>
 public interface IRegisterUserUseCase
 {
+    /// <summary>
+    /// Efetua o registro do usuário.
+    /// </summary>
+    /// <param name="firstName">O primeiro nome do usuário.</param>
+    /// <param name="lastName">O sobrenome do usuário.</param>
+    /// <param name="email">O e-mail do usuário.</param>
+    /// <param name="password">A senha do usuário.</param>
+    /// <returns>Os dados do usuário registrado.</returns>
     Task<User> Register(string firstName, string lastName, string email, string password);
 }
