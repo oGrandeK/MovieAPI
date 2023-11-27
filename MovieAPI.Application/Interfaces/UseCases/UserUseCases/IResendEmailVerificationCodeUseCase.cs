6d74be00b694659c95fc36fb114ee5a17bdec8b2
oGrandeK <kelvimkauam@gmail.com>
2023-11-27 20:05:55 -0300

    Created documentation for IResendEmailVerificationCodeUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IResendEmailVerificationCodeUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IResendEmailVerificationCodeUseCase.cs
@@ -4,5 +4,10 @@ namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
 public interface IResendEmailVerificationCodeUseCase
 {
+    /// <summary>
+    /// Interface para serviços relacionados ao reenvio de código de verificação para o e-mail do usuário.
+    /// </summary>
+    /// <param name="email">O e-mail do usuário a receber o código.</param>
+    /// <returns>As informações do usuário.</returns>
     public Task<User> Resend(string email);
 }
