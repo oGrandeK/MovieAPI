f969eb6a9ac8a5bb415ce665360e8299d530c067
oGrandeK <kelvimkauam@gmail.com>
2023-11-27 20:13:54 -0300

    Created documentation for IVerifyEmailUseCase

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IVerifyEmailUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IVerifyEmailUseCase.cs
@@ -2,7 +2,16 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a verificação do e-mail do <see cref="User"/>.
+/// </summary>
 public interface IVerifyEmailUseCase
 {
+    /// <summary>
+    /// Verifica o e-mail do <see cref="User"/>. 
+    /// </summary>
+    /// <param name="verificationCode">O código de verificação a ser verificado.</param>
+    /// <param name="email">O e-mail que está sendo verificado.</param>
+    /// <returns>As informações do <see cref="User"/> que foi verificado.</returns>
     public Task<User> Verify(string verificationCode, string email);
 }
