b9532c8b59965e2dd20201edf410a12b09304b7f
oGrandeK <kelvimkauam@gmail.com>
2023-11-27 20:08:34 -0300

    Created documentation for IUpdateUserUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/IUpdateUserUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/IUpdateUserUseCase.cs
@@ -2,7 +2,17 @@ using MovieAPI.Domain.Entities;
 
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados a alteração do usuário.
+/// </summary>
 public interface IUpdateUserUseCase
 {
+    /// <summary>
+    /// Atualiza as informações do usuário.
+    /// </summary>
+    /// <param name="id">O ID do usuário a ser buscado.</param>
+    /// <param name="user">As novas informações do usuário.</param>
+    /// <param name="newHash">Nova senha do usuário.</param>
+    /// <returns>O <see cref="User"/> com as informações atualizadas.</returns>
     public Task<User> Execute(int id, User user, string newHash);
 }
