7cadeb4c21a07a117da9779bec013f2c86487a4c
oGrandeK <kelvimkauam@gmail.com>
2023-11-27 20:02:24 -0300

    Created documentation for ILoginUseCase.cs

--- MovieAPI.Application/Interfaces/UseCases/UserUseCases/ILoginUseCase.cs
+++ MovieAPI.Application/Interfaces/UseCases/UserUseCases/ILoginUseCase.cs
@@ -1,6 +1,15 @@
 namespace MovieAPI.Application.Interfaces.UseCases.UserUseCases;
 
+/// <summary>
+/// Interface para serviços relacionados ao login de usuário.
+/// </summary>
 public interface ILoginUseCase
 {
+    /// <summary>
+    /// Efetua o login do usuário.
+    /// </summary>
+    /// <param name="email">O e-mail do usuário.</param>
+    /// <param name="password">A senha do usuário.</param>
+    /// <returns>Um token JWT.</returns>
     Task<string> Login(string email, string password);
 }
